56448c61dc9c8e194f575c141e7a922038f159bb
Daniel Barros <daniel.barros@chartboost.com>
2023-10-16 12:27:59 -0700

    Add comment specifying that the partner support multiple loads for the same placement

--- Source/InMobiAdapter.swift
+++ Source/InMobiAdapter.swift
@@ -105,6 +105,7 @@ final class InMobiAdapter: NSObject, PartnerAdapter {
     /// - parameter request: Information about the ad load request.
     /// - parameter delegate: The delegate that will receive ad life-cycle notifications.
     func makeAd(request: PartnerAdLoadRequest, delegate: PartnerAdDelegate) throws -> PartnerAd {
+        // This partner supports multiple loads for the same partner placement.
         switch request.format {
         case .interstitial, .rewarded:
             return try InMobiAdapterFullscreenAd(adapter: self, request: request, delegate: delegate)
