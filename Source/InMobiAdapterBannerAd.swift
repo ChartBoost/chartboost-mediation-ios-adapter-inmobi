597f7428bbdb17af6b05d119aa655fd51486cd34
Daniel Barros <dani.barros@me.com>
2024-04-17 21:52:32 +0530

    Remove adaptive_banner format, use BannerSize on request model (#52)

--- Source/InMobiAdapterBannerAd.swift
+++ Source/InMobiAdapterBannerAd.swift
@@ -36,7 +36,7 @@ final class InMobiAdapterBannerAd: InMobiAdapterAd, PartnerAd {
         log(.loadStarted)
 
         // Fail if we cannot fit a fixed size banner in the requested size.
-        guard let size = fixedBannerSize(for: request.size ?? IABStandardAdSize) else {
+        guard let size = fixedBannerSize(for: request.bannerSize) else {
             let error = error(.loadFailureInvalidBannerSize)
             log(.loadFailed(error))
             return completion(.failure(error))
@@ -96,13 +96,16 @@ extension InMobiAdapterBannerAd: IMBannerDelegate {
 
 // MARK: - Helpers
 extension InMobiAdapterBannerAd {
-    private func fixedBannerSize(for requestedSize: CGSize) -> CGSize? {
+    private func fixedBannerSize(for requestedSize: BannerSize?) -> CGSize? {
+        guard let requestedSize else {
+            return IABStandardAdSize
+        }
         let sizes = [IABLeaderboardAdSize, IABMediumAdSize, IABStandardAdSize]
         // Find the largest size that can fit in the requested size.
         for size in sizes {
             // If height is 0, the pub has requested an ad of any height, so only the width matters.
-            if requestedSize.width >= size.width &&
-                (size.height == 0 || requestedSize.height >= size.height) {
+            if requestedSize.size.width >= size.width &&
+                (size.height == 0 || requestedSize.size.height >= size.height) {
                 return size
             }
         }
