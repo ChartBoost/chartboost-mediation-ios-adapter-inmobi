7c809e1735aa555930432aaf60aa8066beede2bc
Daniel Barros <dani.barros@me.com>
2024-05-03 15:13:31 -0700

    Adopt new Configuration protocol (#58)

--- Source/InMobiAdapter.swift
+++ Source/InMobiAdapter.swift
@@ -9,27 +9,9 @@ import InMobiSDK
 
 /// The Chartboost Mediation InMobi adapter.
 final class InMobiAdapter: NSObject, PartnerAdapter {
-    /// The version of the partner SDK.
-    var partnerSDKVersion: String {
-        InMobiAdapterConfiguration.partnerSDKVersion
-    }
-
-    /// The version of the adapter.
-    /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
-    /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    var adapterVersion: String {
-        InMobiAdapterConfiguration.adapterVersion
-    }
-
-    /// The partner's unique identifier.
-    var partnerID: String {
-        InMobiAdapterConfiguration.partnerID
-    }
-
-    /// The human-friendly partner name.
-    var partnerDisplayName: String {
-        InMobiAdapterConfiguration.partnerDisplayName
-    }
+    /// The adapter configuration type that contains adapter and partner info.
+    /// It may also be used to expose custom partner SDK options to the publisher.
+    var configuration: PartnerAdapterConfiguration.Type { InMobiAdapterConfiguration.self }
 
     /// The designated initializer for the adapter.
     /// Chartboost Mediation SDK will use this constructor to create instances of conforming types.
@@ -83,7 +65,7 @@ final class InMobiAdapter: NSObject, PartnerAdapter {
     /// - parameter consents: The new consents value, including both modified and unmodified consents.
     /// - parameter modifiedKeys: A set containing all the keys that changed.
     func setConsents(_ consents: [ConsentKey: ConsentValue], modifiedKeys: Set<ConsentKey>) {
-        guard modifiedKeys.contains(partnerID)
+        guard modifiedKeys.contains(configuration.partnerID)
                 || modifiedKeys.contains(ConsentKeys.gdprConsentGiven)
                 || modifiedKeys.contains(ConsentKeys.tcf)
         else {
@@ -100,7 +82,7 @@ final class InMobiAdapter: NSObject, PartnerAdapter {
         }
 
         // GDPR consent
-        let consent = consents[partnerID] ?? consents[ConsentKeys.gdprConsentGiven]
+        let consent = consents[configuration.partnerID] ?? consents[ConsentKeys.gdprConsentGiven]
         switch consent {
         case ConsentValues.granted:
             value[IMCommonConstants.IM_PARTNER_GDPR_CONSENT_AVAILABLE] = String.gdprConsentAvailable
